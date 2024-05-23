9b8650e91920ff6abd0ff39ecaffad21dd71f278
Tingzhen Dong <dtzroger@gmail.com>
2024-05-23 18:57:54 +0800

    update neovim

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,6 @@
 -- vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
 -- twilight
-vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", { noremap = false })
 -- buffers
 vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", { noremap = false, silent = true })
 vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", { noremap = false, silent = true })
@@ -13,12 +13,12 @@ vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", { noremap = false, silent 
 --vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
 --vim.api.nvim_set_keymap("n", "B", "^", {noremap=false})
 --vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", { noremap = true, silent = true })
 --
 -- splits
 --
-vim.api.nvim_set_keymap("n", "<C-W>,", ":vertical resize -10<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<C-W>.", ":vertical resize +10<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<C-W>,", ":vertical resize -10<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-W>.", ":vertical resize +10<CR>", { noremap = true, silent = true })
 vim.keymap.set('n', '<space><space>', "<cmd>set nohlsearch<CR>")
 -- Quicker close split
 vim.keymap.set("n", "<leader>qq", ":q<CR>",
@@ -41,10 +41,10 @@ vim.keymap.set("n", "<leader>ee", "<cmd>GoIfErr<cr>",
 )
 
 -- NvimTree
-vim.api.nvim_set_keymap("n", "tt", ":NvimTreeToggle<CR>", { noremap=true, silent = true })
+vim.api.nvim_set_keymap("n", "tT", ":NvimTreeClose<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "tt", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
 
 -- Terminal
-vim.keymap.set("t", "<C-Space>", "<Esc><C-\\><C-n><C-6>", { noremap = true, silent = true})
 vim.api.nvim_command("autocmd TermOpen * startinsert")             -- starts in insert mode
 vim.api.nvim_command("autocmd TermOpen * setlocal nonumber")       -- no numbers
 vim.api.nvim_command("autocmd TermOpen * setlocal norelativenumber")       -- no numbers
